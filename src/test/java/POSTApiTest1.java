f459407e99af5a6aabf409ea38357a6fafd48b2e
YulShv <yuli.shv@gmail.com>
2023-02-12 08:10:02 +0300

    Initial commit

---
+++ src/test/java/POSTApiTest1.java
@@ -0,0 +1,24 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class POSTApiTest1 {
+    @Test
+    void shouldPostDemoAccount() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("demo account")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("demo account"))
+        ;
+
+    }
+}
